71736a23a09e500ee631fc6292364a5185d7ad4d
Grzegorz Wasilko <grzegorz.wasilko@gmail.com>
2021-08-09 17:23:45 +0200

    Ready for checking

--- mod4-palindrom.py
+++ mod4-palindrom.py
@@ -3,16 +3,23 @@ def checks_for_palindrom (x):
     #funkcja sprawdza czy słowo wysłane jako
     #argumen jet palindromem
     lenght=len(x)
-    print(lenght)
+   # print(f'Testowane słowo {x} posiada  {lenght} liter')
     for i in range (lenght-1):
-        print(x)
-        if x[i]==x[(lenght-1) -i]:
-            print(x[i],x[(lenght-1) -i])
+        if x[i]!=x[(lenght-1) -i]:
+            print(f' słowo {x} nie jest palindormem')
+            return False
+    print(f'\n==>> słowo  {x}  jest  palindromem !!')       
+    return True
             #return True
     #na razie spr tylko czy pierwsze litery sa takie same
     #nie działa na duze i małę litery
     #przecinki i spacje ? 
 
 a='alicja'
-
-checks_for_palindrom(a)
+b='laddy'
+c='kajak'
+d='abccba'
+test=checks_for_palindrom(a)
+test=checks_for_palindrom(b)
+test=checks_for_palindrom(c)
+test=checks_for_palindrom(d)
